46f881b243be685b774cf30ba16230923189c163
MarkOne007 <149779527+MarkOne007@users.noreply.github.com>
2023-11-23 13:38:20 +0100

    Made change for Println

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
-import t1 "time"
+import (
+	"fmt"
+	t1 "time"
+)
 
 func main() {
 
 	var Alfa string
 	Alfa = "Hello there"
 
-	fmt.Print(Alfa)
+	fmt.Println(Alfa)
 	fmt.Println(t1.Now())
 }
